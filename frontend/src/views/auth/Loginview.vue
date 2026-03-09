7d0c39c3078ea24d1c84fadb165c3979fd230591
23f2002602 <23f2002602@ds.study.iitm.ac.in>
2026-03-09 21:18:11 +0530

    Milestone-PPA-V2 Auth-RBAC

---
+++ frontend/src/views/auth/Loginview.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="auth-wrap">
+    <div class="auth-card">
+
+      <h1 class="mb-4">Placement Portal</h1>
+
+      <form @submit.prevent="login">
+
+        <div class="mb-3">
+          <label>Email</label>
+          <input
+            type="email"
+            v-model="email"
+            class="form-control"
+            required
+          />
+        </div>
+
+        <div class="mb-4">
+          <label>Password</label>
+          <input
+            type="password"
+            v-model="password"
+            class="form-control"
+            required
+          />
+        </div>
+
+        <button class="btn-brand w-100">
+          Login
+        </button>
+
+      </form>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      email: "",
+      password: ""
+    }
+  },
+
+  methods: {
+
+    login() {
+
+      console.log("Login clicked")
+
+    }
+
+  }
+
+}
+</script>
